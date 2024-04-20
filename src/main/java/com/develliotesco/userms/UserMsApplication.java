a2d4965354da6be100aa49f944a39f5af7c7c86e
Elliot Escovitch <dev.escovitch.ri@gmail.com>
2024-04-20 13:49:28 -0500

    add database configuration

--- src/main/java/com/develliotesco/userms/UserMsApplication.java
+++ src/main/java/com/develliotesco/userms/UserMsApplication.java
@@ -1,5 +1,7 @@
 package com.develliotesco.userms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserMsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(UserMsApplication.class, args);
+		final Logger log = LoggerFactory.getLogger(UserMsApplication.class);
+		log.info("Bienvenido a demo de microservicios - user ms");
 	}
 
 }
