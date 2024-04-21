6804e96431c2287aa6f0a42bf3ec357deb1b7b60
Elliot Escovitch <dev.escovitch.ri@gmail.com>
2024-04-21 13:07:40 -0500

    add savecar, savebike and getallvehicles with feignclient

--- src/main/java/com/develliotesco/userms/controller/UserController.java
+++ src/main/java/com/develliotesco/userms/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.develliotesco.userms.controller;
 
 import com.develliotesco.userms.entity.User;
+import com.develliotesco.userms.model.Bike;
+import com.develliotesco.userms.model.Car;
 import com.develliotesco.userms.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -8,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
@@ -32,5 +35,34 @@ public class UserController {
     public ResponseEntity<User> save(@RequestBody User user) {
         User savedUser = userService.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+
+    @GetMapping("/cars/{userId}")
+    public ResponseEntity<List<Car>> getCars(@PathVariable int userId) {
+        List<Car> cars = userService.getCars(userId);
+        return ResponseEntity.ok(cars);
+    }
+
+    @GetMapping("/bikes/{userId}")
+    public ResponseEntity<List<Bike>> getBikes(@PathVariable int userId) {
+        List<Bike> bikes = userService.getBikes(userId);
+        return ResponseEntity.ok(bikes);
+    }
+
+    @PostMapping("/savecar/{userId}")
+    public ResponseEntity<Car> saveCar(@PathVariable int userId, @RequestBody Car car) {
+        Car carNew = userService.saveCar(userId, car);
+        return ResponseEntity.status(HttpStatus.CREATED).body(carNew);
+    }
+
+    @PostMapping("/savebike/{userId}")
+    public ResponseEntity<Bike> saveBike(@PathVariable int userId, @RequestBody Bike bike) {
+        Bike bikeNew = userService.saveBike(userId, bike);
+        return ResponseEntity.status(HttpStatus.CREATED).body(bikeNew);
+    }
+
+    @GetMapping("/getallvehicles/{userId}")
+    public ResponseEntity<Map<String, Object>> getAllVehicles(@PathVariable int userId) {
+        return ResponseEntity.ok(userService.getUserAndVehicles(userId));
     }
 }
--- src/main/java/com/develliotesco/userms/service/UserService.java
+++ src/main/java/com/develliotesco/userms/service/UserService.java
@@ -1,17 +1,28 @@
 package com.develliotesco.userms.service;
 
 import com.develliotesco.userms.entity.User;
+import com.develliotesco.userms.feignclients.BikeFeignClient;
+import com.develliotesco.userms.feignclients.CarFeignClient;
+import com.develliotesco.userms.model.Bike;
+import com.develliotesco.userms.model.Car;
 import com.develliotesco.userms.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
+import java.util.*;
 
 @Service
 @AllArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+
+    private final RestTemplate restTemplate;
+
+    private final CarFeignClient carFeignClient;
+
+    private final BikeFeignClient bikeFeignClient;
 
     public List<User> getAll() {
         return userRepository.findAll();
@@ -23,5 +34,35 @@ public class UserService {
 
     public User save(User user) {
         return userRepository.save(user);
+    }
+
+    public List<Car> getCars (int userId) {
+        return restTemplate.getForObject("http://localhost:8002/cars/byuser/" + userId, List.class);
+    }
+
+    public List<Bike> getBikes (int userId) {
+        return restTemplate.getForObject("http://localhost:8003/bikes/byuser/" + userId, List.class);
+    }
+
+    public Car saveCar (int userId, Car car) {
+        car.setUserId(userId);
+        return carFeignClient.save(car);
+    }
+
+    public Bike saveBike (int userId, Bike bike){
+        bike.setUserId(userId);
+        return bikeFeignClient.save(bike);
+    }
+
+    public Map<String, Object> getUserAndVehicles(int userId) {
+        User user = userRepository.findById(userId).orElse(null);
+        List<Car> cars = carFeignClient.getByUserId(userId);
+        List<Bike> bikes = bikeFeignClient.getByUserId(userId);
+
+        Map<String, Object> result = new LinkedHashMap<>();
+        result.put("User", user);
+        result.put("Cars", cars);
+        result.put("Bikes", bikes);
+        return result;
     }
 }
